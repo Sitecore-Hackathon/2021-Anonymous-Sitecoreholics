73c23c52e6409818d516f217dadd1a5d22b6211e
Sebastian Winslow <sebastian.winslow@valtech.com>
2021-03-06 17:49:29 +0100

    added article component

---
+++ src/Feature/ContentComponents/rendering/Models/ArticleModel.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace ContentComponents.Models
+{
+    /// <summary>
+    /// A component used for creating an article component
+    /// </summary>
+    public class ArticleModel
+    {
+        public TextField Title { get; set; }
+
+        public RichTextField Text { get; set; }       
+        
+        public ImageField Image { get; set; }
+
+        public TextField Author { get; set; }
+    }
+}

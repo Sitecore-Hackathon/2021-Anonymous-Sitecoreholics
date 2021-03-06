c399a7005a9c9c6ff77ed5b3f54855d6d5b41415
Sebastian Winslow <sebastian.winslow@valtech.com>
2021-03-06 21:13:55 +0100

    Content, ContentComponent feature and design

---
+++ src/Feature/ContentComponents/rendering/Models/FooterModel.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Speedo.Feature.ContentComponents.Rendering.Models
+{
+    /// <summary>
+    /// A model used for creating a footer
+    /// </summary>
+    public class FooterModel
+    {
+        public RichTextField Copyright { get; set; }
+    }
+}
---
+++ src/Feature/ContentComponents/rendering/Models/HeaderModel.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Speedo.Feature.ContentComponents.Rendering.Models
+{
+    /// <summary>
+    /// A model used for creating a Header
+    /// </summary>
+    public class HeaderModel
+    {
+        public TextField Title { get; set; }
+        public TextField MenuHeader { get; set; }
+    }
+}

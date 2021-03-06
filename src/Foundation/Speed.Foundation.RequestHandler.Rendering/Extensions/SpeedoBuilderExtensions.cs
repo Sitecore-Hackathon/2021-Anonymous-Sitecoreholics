d32f6e6cda92b6e5ac2da04ff82b4c23fdc73b60
Steven Aneel Hasz-Singh <steven.singh@valtech.com>
2021-03-06 13:57:02 +0100

    Initial commit of speedo library for rendering hosts with provider, request handler and extension methods for startup

---
+++ src/Foundation/Speed.Foundation.RequestHandler.Rendering/Extensions/SpeedoBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.LayoutService.Client;
+using Sitecore.LayoutService.Client.Extensions;
+using Speedo.Foundation.RequestHandler.Rendering.Options;
+using Speedo.Foundation.RequestHandler.Rendering.Persistance;
+
+namespace Speedo.Foundation.RequestHandler.Rendering.Extensions
+{
+    public static class SpeedoBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the Speedo Handler to manage Layout Service Requests
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="handlerName"></param>
+        /// <returns></returns>
+        public static ILayoutRequestHandlerBuilder<LayoutRequestHandler> AddSpeedoHandler(
+            this ISitecoreLayoutClientBuilder builder,
+            string handlerName,
+            IConfiguration configuration)
+        {
+            //Register Options
+            builder.Services.Configure<SpeedoOptions>(configuration.GetSection(SpeedoOptions.Key));
+
+            //Register Handler
+            return builder.AddHandler<LayoutRequestHandler>(handlerName);
+        }
+
+        /// <summary>
+        /// Registers a File System provider for Speedo
+        /// </summary>
+        /// <typeparam name="THandler"></typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static ILayoutRequestHandlerBuilder<THandler> WithDiskPersistency<THandler>(
+            this ILayoutRequestHandlerBuilder<THandler> builder)
+            where THandler : ILayoutRequestHandler
+        {
+            //Register the File System Provider
+            builder.Services.AddSingleton<IProvider, FileSystemProvider>();
+
+            return builder;
+        }
+
+        public static ILayoutRequestHandlerBuilder<THandler> WithBlobStoragePersistency<THandler>(
+            this ILayoutRequestHandlerBuilder<THandler> builder)
+            where THandler : ILayoutRequestHandler
+        {
+            //Register Blob Storage Provider
+            builder.Services.AddSingleton<IProvider, BlobStorageProvider>();
+            return builder;
+        }
+    } 
+}
---
+++ src/Foundation/Speed.Foundation.RequestHandler.Rendering/Options/SpeedoOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Speedo.Foundation.RequestHandler.Rendering.Options
+{
+    public class SpeedoOptions
+    {
+        public string LayoutServiceContentFilePath { get; set; }
+        public const string Key = "Speedo";
+    }
+}

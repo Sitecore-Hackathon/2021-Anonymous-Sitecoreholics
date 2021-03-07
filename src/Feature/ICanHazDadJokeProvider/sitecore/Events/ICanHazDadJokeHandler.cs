57dee3cae8d6cee842b17f17924ee9d800ccc76c
Sebastian Winslow <sebastian.winslow@valtech.com>
2021-03-07 01:32:51 +0100

    IcanHazDadJoke feature + test content and minor updates

---
+++ src/Feature/ICanHazDadJokeProvider/sitecore/Events/ICanHazDadJokeHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using Sitecore.ApplicationCenter.Applications;
+using Sitecore.Data.Events;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Events;
+using Sitecore.Shell.Framework.Commands.IDE;
+
+
+namespace Speedo.Feature.ICanHazDadJokeProvider.Events
+{
+    public class ICanHazDadJokeHandler
+    {
+        public void OnItemCreated(object sender, EventArgs e)
+        {
+            var args = e as SitecoreEventArgs;
+            var options = args?.Parameters[0] as ItemCreatedEventArgs;
+            var item = options?.Item;
+            if (item == null)
+            {
+                return;
+            }
+            if (!item.TemplateID.ToString().Equals(Constants.BannerTemplate)) return;
+            
+            var client = new ICanHazDadJokeClient(Constants.LibraryName);
+
+            var image = client.GetRandomJokeAsImageAsync();
+            var joke = client.GetRandomJokeAsync().Result;
+
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                //@TODO: Error when reading media stream - to be fixed if time allows
+                // var mediaItem = UploadImage(image);
+                Sitecore.Diagnostics.Log.Error("Psw2:" + joke.DadJoke, this);
+
+                using (new EditContext(item))
+                {
+                    item.Fields[Constants.BannerTextField].Value = joke.DadJoke;
+                   // AssignMediaItem(item.Fields[Constants.BannerImageField], mediaItem);
+                }
+            }
+        }
+
+        private static MediaItem UploadImage(Task<Stream> image)
+        {
+            var options = new Sitecore.Resources.Media.MediaCreatorOptions();
+            options.Database = Sitecore.Configuration.Factory.GetDatabase("master");
+            options.Language = Sitecore.Globalization.Language.Parse(Sitecore.Configuration.Settings.DefaultLanguage);
+            options.Versioned = Sitecore.Configuration.Settings.Media.UploadAsVersionableByDefault;
+            options.Destination = Constants.MediaLocationJokeImages + Guid.NewGuid().ToString().Replace("{", "").Replace("}","");
+            options.FileBased = Sitecore.Configuration.Settings.Media.UploadAsFiles;
+            var creator = new Sitecore.Resources.Media.MediaCreator();
+            return creator.CreateFromStream(image.Result, ".png", options);
+        }
+
+        private static void AssignMediaItem(ImageField field, Item mediaItem)
+        {
+            field.MediaID = mediaItem.ID;
+            field.SetAttribute("mediapath", mediaItem.Paths.MediaPath);
+            field.SetAttribute("showineditor", "1");
+        }
+    }
+}
---
+++ src/Feature/ICanHazDadJokeProvider/sitecore/IcanHazDadJokeClient.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Speedo.Feature.ICanHazDadJokeProvider
+{
+    public class ICanHazDadJokeClient
+    {
+        /// <summary>
+		/// The URL of the service (https://icanhazdadjoke.com/).
+		/// </summary>
+		public const string BaseUrl = "https://icanhazdadjoke.com/";
+
+        private const string NoUserAgentMessage =
+            "ICanHazDadJoke Kindly request that contact details in form of a website or email is supplied to requests to their free API. See https://icanhazdadjoke.com/api#custom-user-agent for more info.";
+
+        private const string RandomJokeUrl = "/";
+        private const string JokeUrl = "/j/{0}";
+
+        private HttpClient client;
+
+        /// <summary>
+        /// Initializes a client to access the ICanHazDadJoke API
+        /// </summary>
+        /// <param name="site">Site that ICanHazDadJoke can use if to get in touch with the ones using the application</param>
+        /// @TODO: Make as a setting instead if time
+        public ICanHazDadJokeClient(string site)
+        {
+            if (string.IsNullOrWhiteSpace(site))
+                throw new ArgumentException(NoUserAgentMessage, nameof(site));
+
+            client = Init(site);
+        }
+
+        /// <summary>
+        /// Initiate a client to access the ICanHazDadJoke API with
+        /// </summary>
+        /// <param name="site"></param>
+        /// <returns></returns>
+        private HttpClient Init(string site)
+        {
+            client = new HttpClient {BaseAddress = new Uri(BaseUrl)};
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd($"Speedo: https://github.com/Sitecore-Hackathon/2021-Anonymous-Sitecoreholics - ({site})");
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+            return client;
+        }
+
+        /// <summary>
+        /// Gets a random joke from ICanHazDadJoke
+        /// </summary>
+        /// <returns>A random joke deserialized</returns>
+        public async Task<ICanHazDadJokeModel> GetRandomJokeAsync()
+        {
+            var response = await client.GetStringAsync(RandomJokeUrl).ConfigureAwait(false);
+            
+            return JsonConvert.DeserializeObject<ICanHazDadJokeModel>(response);
+        }
+
+        /// <summary>
+        /// Fetches a random joke.
+        /// </summary>
+        /// <returns>A random joke as an png</returns>
+        public async Task<Stream> GetRandomJokeAsImageAsync()
+        {
+            var response = await client.GetStringAsync(RandomJokeUrl).ConfigureAwait(false);
+            var joke = JsonConvert.DeserializeObject<ICanHazDadJokeModel>(response);
+            return await GetJokeAsImageAsync(joke.Id);
+        }
+
+        /// <summary>
+        /// Gets a Joke from ICanHazDadJoke
+        /// </summary>
+        /// <returns>The specified joke deserialized</returns>
+        /// <param name="id">The system id of the Joke at ICanHazDadJoke</param>
+        public async Task<ICanHazDadJokeModel> GetJokeAsync(string id)
+        {
+            var uri = string.Format(JokeUrl, id);
+            var response = await client.GetStringAsync(uri).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<ICanHazDadJokeModel>(response);
+        }
+
+        /// <summary>
+        /// Gets a Joke from ICanHazDadJoke as an Image
+        /// </summary>
+        /// <returns>The specified joke as a png</returns>
+        /// <param name="id">The system id of the Joke at ICanHazDadJoke</param>
+        public async Task<Stream> GetJokeAsImageAsync(string id)
+        {
+            var uri = string.Format(JokeUrl, id + ".png");
+            var response = await client.GetStreamAsync(uri).ConfigureAwait(false);
+            return response;
+        }
+    }
+}
